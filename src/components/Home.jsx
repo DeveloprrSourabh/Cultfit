977ab80e033459ba0d8cbf6c4c48e37ba97e9a75
Sourabh <developrr.sourabh@gmail.com>
2022-11-28 23:24:03 -0600

    add home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
         <div className="video">
-            <video autoplay className='vd' src="https://cdn-images.cure.fit/www-curefit-com/video/upload/c_fill,w_1400,ar_1.77,q_auto:eco,dpr_2,vc_auto,f_auto/video/test/we-are-cult-web.mp4"></video>
+            <video controls autoplay className='vd' src="https://cdn-images.cure.fit/www-curefit-com/video/upload/c_fill,w_1400,ar_1.77,q_auto:eco,dpr_2,vc_auto,f_auto/video/test/we-are-cult-web.mp4"></video>
         </div>
     </>
   )
