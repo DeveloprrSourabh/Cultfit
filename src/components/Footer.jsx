ee46cab3df2e9322bd05dd3c48f080b93db6eb25
Sourabh <developrr.sourabh@gmail.com>
2022-12-16 12:38:30 +0530

    add styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,117 +3,160 @@ import footer from "./styles/footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      {/* FOOTER FIRST CONTAINER---START */}
-      <div className="ftrfs">
-        <div className="mainftr">
-          <div className="ftrftct">
-            <div className="chlftr">
-              <div className="ftrfig">
+    <>
+      <div className="flexqu">
+        {/* FOOTER FIRST CONTAINER---START */}
+        <div className="ftrfs">
+          <div className="mainftr">
+            <div className="ftrftct">
+              <div className="chlftr">
+                <div className="ftrfig">
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_150,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png"
+                    alt=""
+                    className="ftrimg"
+                  />
+                </div>
+                <div className="ftrcation">
+                  At cult.fit, we make group workouts fun, daily food healthy &
+                  tasty, mental fitness easy with yoga & meditation, Medical &
+                  lifestyle care hassle-free.#BeBetterEveryDay
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* ROW COL---START */}
+        <div className="ftrrwcl">
+          <div className="ftrclcl">
+            {/* FIRST ROW---START */}
+            <div className="row ftrfrrow">
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  cult.fit for business
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  partner.fit
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  contact us
+                </a>
+              </div>
+            </div>
+            {/* FIRST ROW---END */}
+            {/* SECOND ROW---START */}
+            <div className="row ftrfrrow">
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  cult.fit franchise
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  blogs
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  privacy policy
+                </a>
+              </div>
+            </div>
+            {/* SECOND ROW---END */}
+
+            {/* THIRD ROW---START */}
+            <div className="row ftrfrrow">
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  corporate partnerships
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  security
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  cult bmi calculator
+                </a>
+              </div>
+            </div>
+            {/* THIRD ROW---END */}
+
+            {/* FOURTH ROW---START */}
+            <div className="row ftrfrrow">
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  t&c for business
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  careers
+                </a>
+              </div>
+              <div className="col-4 ftrfcl">
+                <a href="" className="ftratg">
+                  terms & conditions
+                </a>
+              </div>
+            </div>
+            {/* FOURTH ROW---END */}
+          </div>
+        </div>
+
+        {/* ROW COL---START */}
+        {/* FOOTER FIRST CONTAINER---START */}
+
+        {/* LOGO---START */}
+        <div className="ftrlogo">
+          <div className="whlelogo">
+            <div className="flogo">
+              <a href="" className="ftrflgo">
                 <img
-                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_150,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/vman-and-white-cult-text.png"
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_178,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/download-app/app-store-dark-card-2.png"
                   alt=""
-                  className="ftrimg"
+                  className="ftrlgoimg"
                 />
-              </div>
-              <div className="ftrcation">
-                At cult.fit, we make group workouts fun, daily food healthy &
-                tasty, mental fitness easy with yoga & meditation, Medical &
-                lifestyle care hassle-free.#BeBetterEveryDay
-              </div>
+              </a>
+            </div>
+            <div className="flogo">
+              <a href="" className="ftrflgo">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_178,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/download-app/app-store-dark-card-2.png"
+                  alt=""
+                  className="ftrlgoimg"
+                />
+              </a>
+            </div>
+            <div className="ftricon">
+              <a href="" className="iconftr">
+                X
+              </a>
+              <a href="" className="iconftr">
+                X
+              </a>
+              <a href="" className="iconftr">
+                X
+              </a>
+              <a href="" className="iconftr">
+                X
+              </a>
+              <a href="" className="iconftr">
+                X
+              </a>
             </div>
           </div>
         </div>
+        {/* LOGO---START */}
       </div>
-
-      {/* ROW COL---START */}
-      <div className="ftrrwcl">
-        <div className="ftrclcl">
-          {/* FIRST ROW---START */}
-          <div className="row ftrfrrow">
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                cult.fit for business
-              </a>
-            </div>
-            <div className="col-1 ftrfcl">
-              <a href="" className="ftratg">
-                partner.fit
-              </a>
-            </div>
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                contact us
-              </a>
-            </div>
-          </div>
-          {/* FIRST ROW---END */}
-          {/* SECOND ROW---START */}
-          <div className="row ftrfrrow">
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                cult.fit franchise
-              </a>
-            </div>
-            <div className="col-1 ftrfcl">
-              <a href="" className="ftratg">
-                blogs
-              </a>
-            </div>
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                privacy policy
-              </a>
-            </div>
-          </div>
-          {/* SECOND ROW---END */}
-
-          {/* THIRD ROW---START */}
-          <div className="row ftrfrrow">
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                corporate partnerships
-              </a>
-            </div>
-            <div className="col-1 ftrfcl">
-              <a href="" className="ftratg">
-                security
-              </a>
-            </div>
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                cult bmi calculator
-              </a>
-            </div>
-          </div>
-          {/* THIRD ROW---END */}
-
-          {/* FOURTH ROW---START */}
-          <div className="row ftrfrrow">
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                t&c for business
-              </a>
-            </div>
-            <div className="col-1 ftrfcl">
-              <a href="" className="ftratg">
-                careers
-              </a>
-            </div>
-            <div className="col-2 ftrfcl">
-              <a href="" className="ftratg">
-                terms & conditions
-              </a>
-            </div>
-          </div>
-          {/* FOURTH ROW---END */}
-        </div>
-      </div>
-
-      {/* ROW COL---START */}
-
-      {/* FOOTER FIRST CONTAINER---START */}
-    </div>
+    </>
   );
 };
 
