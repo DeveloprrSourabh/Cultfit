6728774fd65ef59573ec3e008fa3234be3e3c5ed
Sourabh <developrr.sourabh@gmail.com>
2022-12-22 17:30:33 +0530

    add new image

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -4,7 +4,13 @@ const Store = () => {
   return (
 <>
 <div>
-  
+  <div className="strenbr">
+    <div className="nbrstr">
+      <div className="strenbrig">
+        <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_135,ar_3.87,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cultsport-black-logo.svg" alt="" className="str1stre" />
+      </div>
+    </div>
+  </div>
   </div>
 </>   
   )
