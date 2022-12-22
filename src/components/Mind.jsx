25d45ecde80ebe0286f37baed17fe54dcc5f2311
Sourabh <developrr.sourabh@gmail.com>
2022-12-22 17:20:49 +0530

    add new page

--- src/components/Mind.jsx
+++ src/components/Mind.jsx
@@ -1,11 +1,403 @@
-import React from 'react'
+import React from "react";
+import Subnavbar from "./Subnavbar";
+import fitness from "./styles/fitness.css";
 
-const Mind = () => {
+const Fitness = () => {
   return (
-    <div>
-      this is mind page
-    </div>
-  )
-}
+    <>
+      <Subnavbar />
+      {/* MAIN IAMGE---START */}
+      <div className="ft">
+        <div className="mimgft">
+          <img
+            src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_1600,q_auto:eco,dpr_1,f_auto,fl_progressive/image/vm/7996a88c-8958-4bba-9bbd-bbf0433598ee.png"
+            alt=""
+            className="mainftgg"
+          />
+        </div>
+      </div>
+      {/* MAIN IAMGE---END */}
 
-export default Mind
+      {/* BTN---START */}
+      <div className="btft">
+        <div className="btn bn">
+          <a href="" id="ll" className="clf">
+            EXPLORE CULTPASS
+          </a>
+        </div>
+      </div>
+      {/* BTN---END */}
+
+      {/* THREE BOXES---START */}
+      <div style={{ marginTop: "-15rem" }} id="gyo" class="container ftgogyo">
+        <div class="cr">
+          <div class="row">
+            <div class="col-3 ko">
+              <div class="imglp">
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_106,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass.svg"
+                    alt=""
+                    class="rng"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_107,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass-elite-partial.png"
+                    alt=""
+                    class="rng11"
+                  />
+                </div>
+                <p class="rngc">
+                  Unlimited access to group classes, all gyms and at-home
+                  workouts
+                </p>
+              </div>
+            </div>
+            <div class="col-3 ko">
+              <div class="imglp">
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_106,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass.svg"
+                    alt=""
+                    class="rng"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_78,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass-pro-partial.png"
+                    alt=""
+                    class="rng11"
+                  />
+                </div>
+                <p class="rngc">
+                  Unlimited access to all PRO gyms and at-home workouts
+                </p>
+              </div>
+            </div>
+            <div class="col-3 ko">
+              <div class="imglp">
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_106,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass.svg"
+                    alt=""
+                    class="rng"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_85,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass-live-partial-4x.png"
+                    alt=""
+                    class="rng11"
+                  />
+                </div>
+                <p class="rngc">
+                  Unlimited access to group classes, all gyms and at-home
+                  workouts
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* THREE BOXES---END */}
+
+      {/* HEADING---START */}
+      <div className="ftsmhd">
+        <div className="lkh">
+          <h5 className="fthdh5">AT-CENTER</h5>
+        </div>
+      </div>
+
+      {/* HEADING---END */}
+      {/* SECOND HEADING---START */}
+      <div className="ftshd">
+        <div className="sshd">
+          <h2 className="hdh5">Trainer-led group classes</h2>
+        </div>
+      </div>
+      {/* SECOND HEADING---END */}
+      {/* COL BOXES---START */}
+      <div className="ftcolstrt">
+        <div className="allftst">
+          <div className="container">
+            <div className=" ftr1">
+              <div className="humbletruth"></div>
+              <div className="humbletruth">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_300,q_auto:eco,dpr_1,f_auto,fl_progressive//image/vm/5ff5b229-b562-428f-94a9-98da451ed367.png"
+                  alt=""
+                  className="ftrwig"
+                />
+                <button className="ftbt">EXPLORE</button>
+              </div>
+              <div className="humbletruth">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_300,q_auto:eco,dpr_1,f_auto,fl_progressive//image/vm/8009b1d0-4f5e-49e1-bc94-c41da6a5bcc1.png"
+                  alt=""
+                  className="ftrwig"
+                />
+                <button className="ftbt">EXPLORE</button>
+              </div>
+              <div className="humbletruth">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_300,q_auto:eco,dpr_1,f_auto,fl_progressive//image/vm/7bf136db-7217-4733-af9f-1bb8a4155a83.png"
+                  alt=""
+                  className="ftrwig"
+                />
+                <button className="ftbt">EXPLORE</button>
+              </div>
+              <div className="humbletruth">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_300,q_auto:eco,dpr_1,f_auto,fl_progressive//image/vm/7bf136db-7217-4733-af9f-1bb8a4155a83.png"
+                  alt=""
+                  className="ftrwig"
+                />
+                <button className="ftbt">EXPLORE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* COL BOXES---START */}
+
+      {/* HEADING---START */}
+      <div className="ftsmhd">
+        <div className="lkh">
+          <h5 className="fthdh5">AT-HOME</h5>
+        </div>
+      </div>
+
+      {/* HEADING---END */}
+      {/* SECOND HEADING---START */}
+      <div className="ftshd">
+        <div className="sshd">
+          <h2 className="hdh5">
+            Unlimited home workouts with calorie tracking
+          </h2>
+        </div>
+      </div>
+      {/* SECOND HEADING---END */}
+      {/* TWO BIG COL---START */}
+      <div className="twbgcl">
+        <div className="mainbgfcl">
+          <div className="mainstkak">
+            <div className="ig">
+              <img
+                src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_294,q_auto:eco,dpr_1,f_auto,fl_progressive//image/test/brand-logo/cult-pass-elite.png"
+                alt=""
+                className="mainbgtwcl"
+              />
+            </div>
+            <div className="bghdbgbg">
+              <h3 className="bgbgbgh3">
+                Unlimited access to everything in your city
+              </h3>
+            </div>
+            <div className="bgagytyo">
+              <h5 className="bgbgbgh5">Unlimited access to</h5>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">At-center group classes</div>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">At-center group classes</div>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">At-center group classes</div>
+            </div>
+            <div className="twobtanst">
+              <button className="fmmntb">TRY FOR FREE</button>
+              <button className="fmmntb1">BUY NOW</button>
+            </div>
+            <div className="ctha">
+              <siv className="igsedonnow">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_27,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/sku-card-widget/offer.png"
+                  alt=""
+                  className="klimgsaed"
+                />
+              </siv>
+              <div className="hju">SALE ON NOW</div>
+            </div>
+          </div>
+          <div className="hgtuo">
+            <img
+              src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/sku-card-widget/black2.png"
+              alt=""
+              className="ghjty"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* TWO BIG COL---END */}
+
+      {/*SECOND TWO BIG COL---START */}
+      <div style={{ marginTop: "15%" }} className="twbgcl">
+        <div className="mainbgfcl">
+          <div className="hgtuo">
+            <img
+              src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/sku-card-widget/gold2.png"
+              alt=""
+              className="ghjty"
+            />
+          </div>
+          <div className="mainstkak">
+            <div className="ig">
+              <img
+                src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_265,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass-pro.png"
+                alt=""
+                className="mainbgtwcl"
+              />
+            </div>
+            <div className="bghdbgbg">
+              <h3 className="bgbgbgh3">
+                Unlimited access to PRO gyms in your city
+              </h3>
+            </div>
+            <div className="bgagytyo">
+              <h5 className="bgbgbgh5">Unlimited access to</h5>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">All PRO gyms</div>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">At-home live workouts</div>
+            </div>
+            <div className="listgi">
+              <div className="fllitt">
+                <img
+                  src="https://static.cure.fit/assets/images/orangeTick.png"
+                  alt=""
+                  className="ghti"
+                />
+              </div>
+              <div className="licttt">
+                2 Sessions/month at ELITE gyms & group classes
+              </div>
+            </div>
+            <div className="twobtanst">
+              <button className="fmmntb">TRY FOR FREE</button>
+              <button className="fmmntb1">BUY NOW</button>
+            </div>
+            <div className="ctha">
+              <siv className="igsedonnow">
+                <img
+                  src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_27,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/sku-card-widget/offer.png"
+                  alt=""
+                  className="klimgsaed"
+                />
+              </siv>
+              <div className="hju">SALE ON NOW</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* SECOND TWO BIG COL---END */}
+
+      {/* SINGLE BOX---START */}
+      <div id="klihumnll" className="container mainstkak">
+        <div className="ig">
+          <img
+            src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_339,q_auto:eco,dpr_2,f_auto,fl_progressive//image/test/brand-logo/cult-pass-home-1.svg"
+            alt=""
+            className="mainbgtwcl"
+          />
+        </div>
+        <div className="bghdbgbg">
+          <h3 className="bgbgbgh3">Bring the gym home</h3>
+        </div>
+        <div className="bgagytyo">
+          <h5 className="bgbgbgh5">Unlimited access to</h5>
+        </div>
+        <div className="listgi">
+          <div className="fllitt">
+            <img
+              src="https://static.cure.fit/assets/images/orangeTick.png"
+              alt=""
+              className="ghti"
+            />
+          </div>
+          <div className="licttt">At-home workouts</div>
+        </div>
+        <div className="listgi">
+          <div className="fllitt">
+            <img
+              src="https://static.cure.fit/assets/images/orangeTick.png"
+              alt=""
+              className="ghti"
+            />
+          </div>
+          <div className="licttt">Celebrity workouts</div>
+        </div>
+        <div className="listgi">
+          <div className="fllitt">
+            <img
+              src="https://static.cure.fit/assets/images/orangeTick.png"
+              alt=""
+              className="ghti"
+            />
+          </div>
+          <div className="licttt">
+            Goal-based workouts & meditation sessions
+          </div>
+        </div>
+        <div className="twobtanst">
+          <button className="fmmntb">TRY FOR FREE</button>
+          <button className="fmmntb1">BUY NOW</button>
+        </div>
+        <div className="ctha">
+          <div className="igsedonnow">
+            <img
+              src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_27,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/sku-card-widget/offer.png"
+              alt=""
+              className="klimgsaed"
+            />
+          </div>
+          <div className="hju">SALE ON NOW</div>
+        </div>
+      </div>
+      {/* SINGLE BOX---END */}
+    </>
+  );
+};
+
+export default Fitness;
