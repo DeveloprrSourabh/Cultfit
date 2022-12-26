e4b3ee742d5e18343d87423e554c1719a31e6ba6
Sourabh <developrr.sourabh@gmail.com>
2022-12-26 07:33:16 +0530

    add new iten

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -27,6 +27,78 @@ const Product = () => {
       <p className="apifpr2">50% Off</p>
     </div>
   </div>
+  {/* SECOND---START */}
+  <div className="agyafinal">
+    <div className="figprdct">
+      <div className="kkprdct">
+        <span className="ghjuijki">T-SHIRTS</span>
+      </div>
+      <div className="allvw">
+        <a href="" className="vfrtll">VIEW ALL</a>
+      </div>
+    </div>
+    <div className="mnpdctig">
+      <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/mcFkm6K2JzvBewYumhjHk3Se" alt="" className="pdctimghig" />
+    </div>
+    <div className="mmnjhjhjkl">
+        cultsportone
+    </div>
+    <div className="apiheadi">Solid Active Tshirt with Logo</div>
+    <div className="apiagi">
+      <p className="apifpr">₹ 449</p>
+      <p className="apifpr1">₹ 849</p>
+      <p className="apifpr2">50% Off</p>
+    </div>
+  </div>
+  {/* SECOND---END */}
+  {/* THIRD---START */}
+  <div className="agyafinal">
+    <div className="figprdct">
+      <div className="kkprdct">
+        <span className="ghjuijki">T-SHIRTS</span>
+      </div>
+      <div className="allvw">
+        <a href="" className="vfrtll">VIEW ALL</a>
+      </div>
+    </div>
+    <div className="mnpdctig">
+      <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/mcFkm6K2JzvBewYumhjHk3Se" alt="" className="pdctimghig" />
+    </div>
+    <div className="mmnjhjhjkl">
+        cultsportone
+    </div>
+    <div className="apiheadi">Solid Active Tshirt with Logo</div>
+    <div className="apiagi">
+      <p className="apifpr">₹ 449</p>
+      <p className="apifpr1">₹ 849</p>
+      <p className="apifpr2">50% Off</p>
+    </div>
+  </div>
+  {/* THIRD---END */}
+  {/* FOURTH---START */}
+  <div className="agyafinal">
+    <div className="figprdct">
+      <div className="kkprdct">
+        <span className="ghjuijki">T-SHIRTS</span>
+      </div>
+      <div className="allvw">
+        <a href="" className="vfrtll">VIEW ALL</a>
+      </div>
+    </div>
+    <div className="mnpdctig">
+      <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/mcFkm6K2JzvBewYumhjHk3Se" alt="" className="pdctimghig" />
+    </div>
+    <div className="mmnjhjhjkl">
+        cultsportone
+    </div>
+    <div className="apiheadi">Solid Active Tshirt with Logo</div>
+    <div className="apiagi">
+      <p className="apifpr">₹ 449</p>
+      <p className="apifpr1">₹ 849</p>
+      <p className="apifpr2">50% Off</p>
+    </div>
+  </div>
+  {/* FOURTH---END */}
 </div>
     </>
   )
