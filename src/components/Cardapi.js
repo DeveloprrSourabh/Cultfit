5d89e3839a05db1d860930d33ae552593d3ce990
Sourabh <developrr.sourabh@gmail.com>
2022-12-26 06:55:42 +0530

    add product component

---
+++ src/components/Cardapi.js
@@ -0,0 +1,26 @@
+const cardapi = [
+    {
+        id: 1,
+        heading: "BESTSELLERs",
+        subheading: "LOVED BY CULT MEMBERS",
+        imgsrc:
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_300,h_400,ar_0.75,c_fill/dpr_2/cultgear-content/mcFkm6K2JzvBewYumhjHk3Se",
+        about: "FormFit High Impact Ivy Sports Bra",
+        color: "Black",
+    
+        img: [
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/mU1dZAHqT6pjRn55ZWYvq6Mf",
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/bFNaeVGrnJ2wagRkkTgBEv1n",
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/zxZ6xCA2v6VBE9vpMLG5xm1K",
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/qzQmE6e63CujJ9GuzxQtaf89",
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/ZTTm17Y5xfDV1Xp4wU7dqynR",
+          "https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_500,ar_3:4,c_fill/dpr_2/cultgear-content/PUBsxgBzpZckbKDVuT1qA2y4",
+        ],
+        desc: "Your perfect training partner, this sports bra with its hidden stabilizer panels promises extra support and bounce control, especially during high impact activities. The power mesh allows for better ventilation and the hook & eye closure adds to your convenience while offering additional adjustability. Time to be fit with FormFit!",
+        prize: 499,
+        preprize: 899,
+        off: 50,
+        size: ["XS", "S", "M", "L", "XL"],
+        qnty: 0,
+    }
+]
