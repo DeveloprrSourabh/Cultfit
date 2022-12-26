afbe4332c6793d4780e63ef973bba19b5cad93c8
Sourabh <developrr.sourabh@gmail.com>
2022-12-26 07:06:00 +0530

    add new img

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Product = () => {
   return (
     <>
-
+<div className="productj">
+  <div className="agyafinal">
+    <div className="figprdct">
+      <div className="kkprdct">
+        <span className="ghjuijki">T-SHIRTS</span>
+      </div>
+    </div>
+  </div>
+</div>
     </>
   )
 }
