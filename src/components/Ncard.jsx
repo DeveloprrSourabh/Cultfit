bb6a88393019b35b608223a19f30cde3c9031d23
Sourabh <developrr.sourabh@gmail.com>
2022-12-28 04:23:10 +0530

    add new data in the card

---
+++ src/components/Ncard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cardapi from "./Cardapi";
+import Product from "./Product";
+
+const Ncard = (val) => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Ncard
