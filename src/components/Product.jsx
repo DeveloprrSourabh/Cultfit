bbb331c81f7b073fd6bde90085a374fa089e4886
Sourabh <developrr.sourabh@gmail.com>
2022-12-28 06:01:08 +0530

    add new content

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ function Product(props) {
   // console.log(props);
   return (
     <>
-      <div key={props.id} className="apihgth">
+      {/* <div key={props.id} className="apihgth">
         <div className="figprdct">
           <div className="kkprdct">
             <span className="ghjuijki">{props.heading}</span>
@@ -16,19 +16,29 @@ function Product(props) {
             </a>
           </div>
         </div>
-      </div>
+        
+      </div> */}
       <a className="flex row productj">
         {/* FIRST---START */}
-        <div class="card" style="width: 18rem;">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwZ34AwObpfWxd3-nusuCkTymEvcfIwbDcK_rY9sFv&s" class="card-img-top" alt="..." />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        <a href="" className="col-3 mmkkl">
+          <div className="agyafinal">
+            <div className="mnpdctig">
+              <img src={props.imgsrc} alt="" className="pdctimghig" />
+            </div>
+            <div className="mmnjhjhjkl">cultsportone</div>
+            <div className="apiheadi">{props.about}</div>
+            <div className="apiagi">
+              <p className="apifpr">₹{props.prize}</p>
+              <p className="apifpr1"> ₹{props.preprize}</p>
+              <p className="apifpr2"> {props.off}% off</p>
+            </div>
+          </div>
+        </a>
         {/* FIRST---END */}
       </a>
+
+
+      
     </>
   );
 }
