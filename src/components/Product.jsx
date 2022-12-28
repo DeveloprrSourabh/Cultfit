8a59b4dbcf424f77b5bdf3a94340d8b10b621619
Sourabh <developrr.sourabh@gmail.com>
2022-12-28 07:26:11 +0530

    add font family

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,10 +6,10 @@ function Product(props) {
   // console.log(props);
   return (
     <>
-        <div key={props.id} className="stylishname">
-        <span className="ghnt">{props.heading}</span>
-        <a className="thik">{props.subheading}</a>
-      </div>
+          <div key={props.id} className="stylishname">
+          <span className="ghnt">{props.heading}</span>
+          <a className="thik">{props.subheading}</a>
+        </div>
       <div className="card">
         <div className="flex">
           <NavLink to={`/cultsport/${props.id}`}>
