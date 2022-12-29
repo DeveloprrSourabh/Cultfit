91505ba71cfb66d8185a3046257823da900c3f28
Sourabh <developrr.sourabh@gmail.com>
2022-12-29 06:13:19 +0530

    add styling to card

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,11 +5,12 @@ import "./styles/product.css";
 function Product(props) {
   // console.log(props);
   return (
-    <>  
-          <div key={props.id} className="stylishname">
-          <span className="ghnt">{props.heading}</span>
-          <a className="thik">{props.subheading}</a>
-        </div>
+    <>
+     <div className="prblrm">
+     <div key={props.id} className="stylishname">
+        <span className="ghnt">{props.heading}</span>
+        <a className="thik">{props.subheading}</a>
+      </div>
       <div className="card">
         <div className="flex">
           <NavLink to={`/cultsport/${props.id}`}>
@@ -20,20 +21,15 @@ function Product(props) {
               <div className="makecarddiv2">CULTSPORTONE</div>
               <div className="makecarddiv3">{props.about}</div>
               <div className="makecarddiv4">
-                <p className="makecardp1">
-                  ₹{props.prize}
-                </p>
-                <p className="makecardp2">
-                  ₹{props.preprize}
-                </p>
-                <p className="makecardp3">
-                  {props.off}% off
-                </p>
+                <p className="makecardp1">₹{props.prize}</p>
+                <p className="makecardp2">₹{props.preprize}</p>
+                <p className="makecardp3">{props.off}% off</p>
               </div>
             </div>
           </NavLink>
         </div>
       </div>
+     </div>
     </>
   );
 }
