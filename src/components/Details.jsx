cd8959a9c779b96be94d1d113d641309e1f0e3ff
Sourabh <developrr.sourabh@gmail.com>
2023-01-03 06:07:56 +0530

    add styling to img

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,39 +1,37 @@
 import React from "react";
-import {useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Cardapi from "./Cardapi";
 import Storenav from "./Storenav";
-import "./styles/Details.css"
-
+import "./styles/Details.css";
 
 const Details = () => {
-
   const proid = useParams();
   const proDetail = Cardapi.filter((x) => x.id == proid.id);
   const product = proDetail[0];
 
   return (
     <>
-
-<Storenav />
-<div className="dtpg">
-  <div className="igstpg">
-  <div className="igmgdtg">
-    <img src={product.img[0]} alt="" className="dtigmgnhj" />
-  </div>
-  <div className="igmgdtg">
-    <img src={product.img[1]} alt="" className="dtigmgnhj" />
-  </div>
-  <div className="igmgdtg">
-    <img src={product.img[2]} alt="" className="dtigmgnhj" />
-  </div>
-  <div className="igmgdtg">
-    <img src={product.img[3]} alt="" className="dtigmgnhj" />
-  </div>
-  </div>
-</div>
+      <Storenav />
+      <div className="dtpg">
+        <div className="igstpg">
+          <div className="ghftytfff">
+          <div className="igmgdtg">
+            <img src={product.img[0]} alt="" className="dtigmgnhj" />
+          </div>
+          <div className="igmgdtg">
+            <img src={product.img[1]} alt="" className="dtigmgnhj" />
+          </div>
+          <div className="igmgdtg">
+            <img src={product.img[2]} alt="" className="dtigmgnhj" />
+          </div>
+          <div className="igmgdtg">
+            <img src={product.img[3]} alt="" className="dtigmgnhj" />
+          </div>
+          </div>
+        </div>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Details
+export default Details;
