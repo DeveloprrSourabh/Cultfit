ba4df9e24807778adb906d99dd5d5711c029500b
Sourabh <developrr.sourabh@gmail.com>
2023-01-03 06:15:17 +0530

    add size of product

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -34,13 +34,29 @@ const Details = () => {
             <div className="hhjdghf">CULTSPORTONE</div>
           </div>
           <div className="trreehd">
-            <div className="mothd">
-            Solid Active Tshirt with Logo
-            </div>
+            <div className="mothd">Solid Active Tshirt with Logo</div>
             <div className="prizertr">
               <p className="fprtr">₹ 539</p>
               <p className="fprtr1">₹ 899</p>
               <p className="fprtr2">40 % Off</p>
+            </div>
+            <div className="paraf">
+              <p className="trparaf">
+                Update your sportswear essentials, with this practical and
+                versatile t-shirt from Cultsport One. It's designed in a
+                comfortable fit and with all performance features. The
+                breathable fabric is moisture-wicking and quick-drying, to keep
+                you cool and collected no matter how hard you train.
+              </p>
+            </div>
+            <div className="sunobtn">
+              <div className="hgjuyhbtn">
+                BEST SELLER
+              </div>
+            </div>
+            <div className="titleconti">
+              <div className="chsesze">Choose Size</div>
+              <div className="szeguide">Size Guide</div>
             </div>
           </div>
         </div>
