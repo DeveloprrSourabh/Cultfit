23baa507051eaad547f9f161d0bf7a2ffcafee0f
Sourabh <developrr.sourabh@gmail.com>
2023-01-04 18:16:27 +0530

    add new height

--- src/components/Sideb.js
+++ src/components/Sideb.js
@@ -76,6 +76,48 @@ const Sideb = ({ sidebar }) => {
             />
           </div>
         </div>
+        {/* SECOND--CART */}
+        <div className="prdtcrt">
+          <div className="piccrt">
+            <img
+              src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_125,ar_5:6,c_fit/dpr_2/cultgear-content/vtg2iiscHQwMC3K1Dd6C8ztf"
+              alt=""
+              className="iiiopicrt"
+            />
+          </div>
+          <div className="vcationcrt">
+            <div className="fcrttion">Urban Terrain</div>
+            <div className="hcrt">
+              {" "}
+              <div className="hjyitcrt2">UT1001, Blue, Steel MTB 27.5...</div>
+            </div>
+            <div className="brtcrt">
+              <div className="sizecrt">Size:ALL</div>
+              <div className="bhvccrt">₹ {12999*count}</div>
+              <div className="hjycrtpm">
+                <img onClick={decNum}
+                  src="https://static.cure.fit/assets/images/minus.svg"
+                  alt=""
+                  className="minusimg"
+                />
+
+                <div className="countgdhy">{count}</div>
+                <img onClick={incNum}
+                  src="https://static.cure.fit/assets/images/plus.svg"
+                  alt=""
+                  className="minusimg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="kata">
+            <img
+              src="https://static.cure.fit/assets/images/modal-close.svg"
+              className="imgktgt"
+            />
+          </div>
+        </div>
+        {/* SECOND---CART---END */}
       </div>
     </>
   );
