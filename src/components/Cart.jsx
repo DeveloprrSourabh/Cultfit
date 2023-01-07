3b9cec81958edada19c855d14a7a5944d5787e3c
Sourabh <developrr.sourabh@gmail.com>
2023-01-07 11:24:52 +0530

    add new styling in cart item

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,68 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Cart.css";
+import Cardapi from "./Cardapi";
 
 const Cart = () => {
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState(Cardapi);
   return (
     <>
       <div className="check_page">
         <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <div className="main_div">
-                <div className="all_item">
-                  <div className="main_item">
-                    <div className="both_item">
-                      <div className="fir_img">
-                        <img
-                          src="https://cdn-images.cure.fit/www-curefit-com/image/upload/fl_progressive,f_auto,q_auto:eco,w_125,ar_5:6,c_fit/dpr_2/cultgear-content/gyEKit98fH6VGSZ2jv1fFkSm"
-                          alt=""
-                          className="img_start"
-                        />
-                      </div>
-                      <div className="cation_str">
-                        <div className="cation_flx">
-                          <div className="fr_hd">cultsportone</div>
-                          <div className="lambi">
-                            <div className="lmbi_str">
-                              Textured Performance Tshirt with Logo
-                            </div>
-                          </div>
-                          <div className="fbtn_st">
-                            <div className="fbtn3">
-                              <div className="mainbtn_str">BEST SELLER</div>
-                            </div>
-                          </div>
-                          <div className="size_str">Size: L</div>
-                          <div className="price_tr">₹ 499</div>
-                          <div className="btn34_stret">
-                            <div className="pink_str">
-                              <img
-                                src="https://static.cure.fit/assets/images/minus.svg"
-                                alt=""
-                                className="minus_str"
-                              />
-                              <div className="count_str">1</div>
-                              <img
-                                src="https://static.cure.fit/assets/images/plus.svg"
-                                alt=""
-                                className="plus_str"
-                              />
+          <div className="p">
+            <div className="co">
+              {list.map((e) => {
+                return (
+                  <>
+                    <div className="row">
+                      <div className="col-12">
+                        <div className="main_div">
+                          <div className="all_item">
+                            <div className="main_item">
+                              <div className="both_item">
+                                <div className="fir_img">
+                                  <img
+                                    src={e.imgsrc}
+                                    alt=""
+                                    className="img_start"
+                                  />
+                                </div>
+                                <div className="cation_str">
+                                  <div className="cation_flx">
+                                    <div className="fr_hd">cultsportone</div>
+                                    <div className="lambi">
+                                      <div className="lmbi_str">
+                                        {e.about}
+                                      </div>
+                                    </div>
+                                    <div className="fbtn_st">
+                                      <div className="fbtn3">
+                                        <div className="mainbtn_str">
+                                          BEST SELLER
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div className="size_str">Size: L</div>
+                                    <div className="price_tr">₹ {e.prize}</div>
+                                    <div className="btn34_stret">
+                                      <div className="pink_str">
+                                        <img
+                                          src="https://static.cure.fit/assets/images/minus.svg"
+                                          alt=""
+                                          className="minus_str"
+                                        />
+                                        <div className="count_str">{count}</div>
+                                        <img
+                                          src="https://static.cure.fit/assets/images/plus.svg"
+                                          alt=""
+                                          className="plus_str"
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <img
+                                  src="https://static.cure.fit/assets/images/modal-close.svg"
+                                  alt=""
+                                  className="dele_item"
+                                />
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <img
-                        src="https://static.cure.fit/assets/images/modal-close.svg"
-                        alt=""
-                        className="dele_item"
-                      />
+                      {/* SECOND_COL---START */}
                     </div>
-                  </div>
-                </div>
-              </div>
+                  </>
+                );
+              })}
             </div>
-            {/* SECOND_COL---START */}
-            <div className="col-6">
+            <div className="jki">
+            <div className="col-12">
               <div className="second_str">
                 <div className="inpt_str">
                   <div className="impt_str2">
@@ -101,7 +117,7 @@ const Cart = () => {
               <div className="nex_khali">
                 <div className="column">
                   <div className="row col-md-12">
-                    <div className="col-xs-1 col-md-1">
+                    <div className="col-xs-1 col-md-1 no_pading">
                       <img
                         src="https://static.cure.fit/assets/images/timePink.svg"
                         alt=""
@@ -109,7 +125,10 @@ const Cart = () => {
                       />
                     </div>
                     <div className="col-xs-11 col-md-11">
-                      <div className="row col-md-12 col-xs-12">
+                      <div
+                        style={{ fontSize: "16px" }}
+                        className="row col-md-12 col-xs-12"
+                      >
                         <div className="hed_agi">Estimated Delivery</div>
                         <div className="moto_agyo">
                           Please fill your address to see the estimated delivery
@@ -128,10 +147,10 @@ const Cart = () => {
                   <p className="tr_vl">₹999</p>
                 </div>
                 <div className="lk_str">
-                  <p className="p_str">(-) Flat 50% Off</p>
-                  <p className="tr_vl">₹999</p>
+                  <p className="p_str2">(-) Flat 50% Off</p>
+                  <p className="tr_vl2">₹999</p>
                 </div>
-                <div className="lk_str">
+                <div className="lk_str1">
                   <p className="alg_str">Total Payable</p>
                   <p className="vlfg">₹499</p>
                 </div>
@@ -139,6 +158,7 @@ const Cart = () => {
               <div className="chot_bt">
                 <button className="sdr">Select Address</button>
               </div>
+            </div>
             </div>
           </div>
         </div>
