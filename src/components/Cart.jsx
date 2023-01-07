a4377df887969fa7c6db5f4ed81a879437c2ff28
Sourabh <developrr.sourabh@gmail.com>
2023-01-07 08:50:39 +0530

    add new items in cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,7 +23,9 @@ const Cart = () => {
                         <div className="cation_flx">
                           <div className="fr_hd">cultsportone</div>
                           <div className="lambi">
-                            <div className="lmbi_str">Textured Performance Tshirt with Logo</div>
+                            <div className="lmbi_str">
+                              Textured Performance Tshirt with Logo
+                            </div>
                           </div>
                           <div className="fbtn_st">
                             <div className="fbtn3">
@@ -34,9 +36,58 @@ const Cart = () => {
                           <div className="price_tr">₹ 499</div>
                           <div className="btn34_stret">
                             <div className="pink_str">
-                              <img src="https://static.cure.fit/assets/images/minus.svg" alt="" className="minus_str" />
+                              <img
+                                src="https://static.cure.fit/assets/images/minus.svg"
+                                alt=""
+                                className="minus_str"
+                              />
                               <div className="count_str">1</div>
-                              <img src="https://static.cure.fit/assets/images/plus.svg" alt="" className="plus_str" />
+                              <img
+                                src="https://static.cure.fit/assets/images/plus.svg"
+                                alt=""
+                                className="plus_str"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <img
+                        src="https://static.cure.fit/assets/images/modal-close.svg"
+                        alt=""
+                        className="dele_item"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="second_str">
+                <div className="inpt_str">
+                  <div className="impt_str2">
+                    <div className="row roe_mao">
+                      <div className="col-xs-1 col-md-1">
+                        <img
+                          src="https://static.cure.fit/assets/images/locationPink.svg"
+                          alt=""
+                          className="img_seccol"
+                        />
+                      </div>
+                      <div className="col-xs-11 col-md-11">
+                        <div className="row col-xs-12 col-md-12">
+                          <div className="checkout_adres">
+                            <div className="row">
+                              <div className="col-xs-12">
+                                <div className="chk_ress"></div>
+                              </div>
+                              <div className="col-xs-12">
+                                <div className="agyo_adress">
+                                  <div className="seect_adress">
+                                    Please add address
+                                  </div>
+                                  <div className="prima_clr">Add/Modify</div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -44,9 +95,26 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
+                <div className="khali_pilik"></div>
+              </div>
+              <div className="nex_khali">
+                <div className="column">
+                  <div className="row col-md-12">
+                    <div className="col-xs-1 col-md-1">
+                      <img src="https://static.cure.fit/assets/images/timePink.svg" alt="" className="jhy_img" />
+                    </div>
+                    <div className="col-xs-11 col-md-11">
+                      <div className="row col-md-12 col-xs-12">
+                        <div className="hed_agi">Estimated Delivery</div>
+                        <div className="moto_agyo">Please fill your address to see the estimated delivery date</div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="khali_pilik"></div>
               </div>
             </div>
-            <div className="col-6"></div>
           </div>
         </div>
       </div>
