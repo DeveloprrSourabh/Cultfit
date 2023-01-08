98110e5061143291e80926eec2254048107f7da1
Sourabh <developrr.sourabh@gmail.com>
2023-01-08 08:23:50 +0530

    add new styling in cart component

---
+++ src/components/Brand.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+
+
+const Brand = ({  id,
+    heading,
+    imgsrc,
+    about,
+    color,
+    desc,
+    prize,
+    preprize,
+    off,
+    sidebar,}) => {
+
+    const [count, setCount] = useState(0);
+
+    const incNum = () => {
+      if (count < 9) {
+        setCount(count + 1);
+      } else {
+        alert("You can Exceed The Limit!");
+      }
+    };
+  
+    const decNum = () => {
+      if (count > 0) {
+        setCount(count - 1);
+      } else {
+        setCount(0);
+      }
+    };
+
+
+  return (
+    <>
+     <div className="co">
+              <div className="row">
+                <div className="col-12">
+                  <div className="main_div">
+                    <div className="all_item">
+                      <div className="main_item">
+                        <div className="both_item">
+                          <div className="fir_img">
+                            <img src={imgsrc} alt="" className="img_start" />
+                          </div>
+                          <div className="cation_str">
+                            <div className="cation_flx">
+                              <div className="fr_hd">cultsportone</div>
+                              <div className="lambi">
+                                <div className="lmbi_str">{about}</div>
+                              </div>
+                              <div className="fbtn_st">
+                                <div className="fbtn3">
+                                  <div className="mainbtn_str">BEST SELLER</div>
+                                </div>
+                              </div>
+                              <div className="size_str">Size: L</div>
+                              <div className="price_tr">₹ {prize}</div>
+                              <div className="btn34_stret">
+                                <div className="pink_str">
+                                  <img
+                                    onClick={decNum}
+                                    src="https://static.cure.fit/assets/images/minus.svg"
+                                    alt=""
+                                    className="minus_str"
+                                  />
+                                  <div className="count_str">{count}</div>
+                                  <img
+                                    onClick={incNum}
+                                    src="https://static.cure.fit/assets/images/plus.svg"
+                                    alt=""
+                                    className="plus_str"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <img
+                            src="https://static.cure.fit/assets/images/modal-close.svg"
+                            alt=""
+                            className="dele_item"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    </>
+  )
+}
+
+export default Brand
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,98 +1,32 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./styles/Cart.css";
 import Cardapi from "./Cardapi";
+import Brand from "./Brand";
+import { CartContext } from "./Store";
 
 const Cart = () => {
-  const [count, setCount] = useState(0);
-  const [list, setList] = useState(Cardapi);
+  const [item, setItem] = useState(Cardapi);
 
-  const inc = () => {
-    if (count < 9) {
-      setCount(count + 1);
-    } else {
-      alert("You Exceed The Limit!");
-    }
-  };
-  const dec = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    } else {
-      setCount(0);
-    }
-  };
+
 
   return (
     <>
       <div className="check_page">
         <div className="container">
           <div className="p">
-            <div className="co">
-              {list.map((e) => {
-                return (
-                  <>
-                    <div className="row">
-                      <div className="col-12">
-                        <div className="main_div">
-                          <div className="all_item">
-                            <div className="main_item">
-                              <div className="both_item">
-                                <div className="fir_img">
-                                  <img
-                                    src={e.imgsrc}
-                                    alt=""
-                                    className="img_start"
-                                  />
-                                </div>
-                                <div className="cation_str">
-                                  <div className="cation_flx">
-                                    <div className="fr_hd">cultsportone</div>
-                                    <div className="lambi">
-                                      <div className="lmbi_str">{e.about}</div>
-                                    </div>
-                                    <div className="fbtn_st">
-                                      <div className="fbtn3">
-                                        <div className="mainbtn_str">
-                                          BEST SELLER
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div className="size_str">Size: L</div>
-                                    <div className="price_tr">₹ {e.prize}</div>
-                                    <div className="btn34_stret">
-                                      <div className="pink_str">
-                                        <img
-                                          onClick={dec}
-                                          src="https://static.cure.fit/assets/images/minus.svg"
-                                          alt=""
-                                          className="minus_str"
-                                        />
-                                        <div className="count_str">{count}</div>
-                                        <img
-                                          onClick={inc}
-                                          src="https://static.cure.fit/assets/images/plus.svg"
-                                          alt=""
-                                          className="plus_str"
-                                        />
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                                <img
-                                  src="https://static.cure.fit/assets/images/modal-close.svg"
-                                  alt=""
-                                  className="dele_item"
-                                />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      {/* SECOND_COL---START */}
-                    </div>
-                  </>
-                );
-              })}
-            </div>
+           
+          {item.map((curItem) => {
+                    return (
+                      <>
+                      <Brand key={curItem.id} {...curItem}/>
+                    
+                    </>
+                    );
+                   
+                  })}
+
+
+           
             <div className="jki">
               <div className="col-12">
                 <div className="second_str">
