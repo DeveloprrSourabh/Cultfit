5e158428b251e835a2cdbc132e37b4169dc058b3
Sourabh <developrr.sourabh@gmail.com>
2023-01-11 15:18:39 +0530

    add new content

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,38 +8,35 @@ import Whole from "./Whole";
 const ContextBrand = createContext(Cardapi);
 
 const secinitialState = {
-  items:Cardapi,
+  items: Cardapi,
   totalAmounts: 0,
   totalItems: 0,
 };
 
- 
+const Cart = ({ children }) => {
+  const [secState, dispatchs] = useReducer(secreducer, secinitialState);
 
-const Cart = ({children}) => {
-
-const [secState, dispatchs] = useReducer(secreducer, secinitialState);
-
- // Increment The Item
- const increments = (id) =>{
-  return dispatchs({
-    type:"INCREMENTS",
-    payload:id,
-  })
+  // Increment The Item
+  const increments = (id) => {
+    return dispatchs({
+      type: "INCREMENTS",
+      payload: id,
+    });
   };
 
   // Decrement The Item
-const decrements = (id) =>{
-  return dispatchs({
-    type:"DECREMENTS",
-    payload:id,
-  })
+  const decrements = (id) => {
+    return dispatchs({
+      type: "DECREMENTS",
+      payload: id,
+    });
   };
   return (
     <>
-    <ContextBrand.Provider value={{...secState,increments,decrements}}>
-    <Whole />
-    {children}
-    </ContextBrand.Provider>
+      <ContextBrand.Provider value={{ ...secState, increments, decrements }}>
+        <Whole />
+        {children}
+      </ContextBrand.Provider>
     </>
   );
 };
