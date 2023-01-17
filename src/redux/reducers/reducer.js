ffdfb0b3f86ed69fb85692390cdde66043a283d3
Sourabh <developrr.sourabh@gmail.com>
2023-01-17 15:49:44 +0530

    add new store

---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,19 @@
+import { reducer } from "../../components/reducer";
+
+const INIT_STATE = {
+  carts: [],
+};
+
+export const cartreducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case "ADD_CART":
+      return {
+        ...state,
+        carts: [...state.carts, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
+export default reducer;
