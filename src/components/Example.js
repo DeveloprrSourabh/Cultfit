b85feb115abc0221408dca1f3c2f486db7228ecb
Sourabh <developrr.sourabh@gmail.com>
2023-01-24 19:04:23 +0530

    completed login logout interface

--- src/components/Example.js
+++ src/components/Example.js
@@ -31,6 +31,7 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "./styles/Login.css";
+import Loginbtn from "./Loginbtn"
 
 function Example() {
   const [show, setShow] = useState(false);
@@ -48,61 +49,96 @@ function Example() {
           className="lginkr"
         />
       </div>
-      <Modal className="hu" show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <div className="fbexiti"> Existing user?</div>
-            <div className="loginbtnexiti">
-              <div className="btnexilgin">LOGIN TO CONTINUE</div>
-            </div>
-            <div className="flxprecxit">
-              <div className="flatu"></div>
-              <div className="nofltu">or</div>
-              <div className="flatu"></div>
-            </div>
-            <div className="shpiing_dtl">Enter Shipping Details</div>
-            <div className="maincontentexiting">
-              <div className="nameofec">
-                <div className="agnameexi">Full Name</div>
-                <input className="inutex1" type="text" />
-              </div>
-              <div className="mobilenum">
-                <div className="gghhexinum">Mobile Numeber</div>
-                <div className="ytwvfff3w">
-                  <div className="style-prefix-1q76qitt">
-                    <div className="ferex">
-                      +91{" "}
-                      <img
-                        src="https://static.cure.fit/assets/images/arrow-clp.svg"
-                        alt=""
-                        className="exiting21"
+      <div className="bda">
+        <Modal className="hu" show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <div className="mainexiti">
+                <div className="byutghgshhgj">
+                  <div className="fbexiti"> Existing user?</div>
+                  <div className="loginbtnexiti">
+                  <Loginbtn />
+                  </div>
+                  <div className="flxprecxit">
+                    <div className="flatu"></div>
+                    <div className="nofltu">or</div>
+                    <div className="flatu"></div>
+                  </div>
+                  <div className="shpiing_dtl">Enter Shipping Details</div>
+                  <div className="maincontentexiting">
+                    <div className="floatLabe nameofec">
+                      <div className="agnameexi">Full Name</div>
+                      <input className="samein" type="text" />
+                    </div>
+                    <div className="floatLabe ">
+                      <div className="agnameexi">Mobile Numeber</div>
+                      <div className="ytwvfff3w">
+                        <div className="style-prefix-1q76qitt">
+                          <div className="ferex">
+                            +91{" "}
+                            <img
+                              src="https://static.cure.fit/assets/images/arrow-clp.svg"
+                              alt=""
+                              className="exiting21"
+                            />
+                          </div>
+                        </div>
+                        <input type="number" className="samein" />
+                      </div>
+                    </div>
+                    <div className="floatLabe emailwla">
+                      <div className="agnameexi">Email</div>
+                      <input type="email" className="samein" />
+                    </div>
+                    <div className="floatLabe pincwla">
+                      <div className="agnameexi">Pincode</div>
+                      <input
+                        type="number"
+                        inputMode="Numeric"
+                        maxLength={6}
+                        pattern="[0-9]*"
+                        className="samein"
                       />
                     </div>
+                    <div className="floatLabe adress">
+                      <div className="agnameexi">Address</div>
+                      <input placeholder="Address" type="text" className="samein" />
+                    </div>
+                    <div className="floatLabe city">
+                      <div className="agnameexi">City</div>
+                      <input
+                        placeholder="City"
+                        type="text"
+                        className="samein"
+                      />
+                    </div>
+                    <div className="floatLabe  state">
+                      <div className="agnameexi">State</div>
+                      <input
+                        placeholder="State"
+                        type="text"
+                        className="samein"
+                      />
+                    </div>
+                    <div className="dip">
+                      <div className="just">
+                        <div className="paralin">
+                          Receive order updates and reminders on Whatsapp
+                        </div>
+                      </div>
+                    </div>
+                    <div className="buttondis">
+                      <button className="disbtn">
+                        <div className="btncontent">PROCEED TO PAY</div>
+                      </button>
+                    </div>
                   </div>
-                  <input type="number" className="exiting2in" />
                 </div>
               </div>
-              <div className="emailwla">
-                <div className="nameofemail">Email</div>
-                <input type="email" className="emailnput" />
-              </div>
-              <div className="pincwla">
-                <div className="nameofpin">Pincode</div>
-                <input type="number" min="0" inputMode="Numeric" maxLength={6}  pattern="[0-9]*" className="emailnput" />
-              </div>
-            </div>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+            </Modal.Title>
+          </Modal.Header>
+        </Modal>
+      </div>
     </>
   );
 }
